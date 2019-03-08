beb75fa93e7c0f40a26ad802584191da1e3e0de9
Prashanth <prashanthr036@gmail.com>
2019-03-08 12:20:13 -0600

    update

--- app.js
+++ app.js
@@ -8,6 +8,7 @@ app.get('/', function (req, res) {
   res.send('Welcome home!')
 })
 
+
 app.get('/hello', (req, res) => {
   res.send('Hello World!')
 })
@@ -17,12 +18,26 @@ app.get('/big',  (req, res) =>{
 })
 
 app.get('/greeting/:id',  (req, res) =>{
-  res.send('Hello! The id was ' + req.params.id)
+  res.send('<h2> Hello! The given parameter id was ' +req.params.id+'</h2>'+
+  '<br> <h2> The given id multiplied by 100 gives ' + req.params.id*100+'</h2>'+
+  '<br> <h2> The given id square value gives ' + req.params.id*req.params.id+'</h2>'+
+  '<br> <h2> The given id cube value gives ' + req.params.id*req.params.id*req.params.id+'</h2>'
+ )
+  
+ 
 })
 
 app.get('/yo/:buddy',  (req, res) =>{
   res.send('<h1>Yo, ' + req.params.buddy + '!</h1>')
 })
+
+
+app.use(express.static('public'));
+
+app.get('/images/backg.jpg', function (req, res) {
+   res.send(' ')
+})
+
 
 // handle non-existant routes
 app.use((req, res, next) => {
